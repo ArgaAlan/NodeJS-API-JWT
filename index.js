ebb28f14731e88a2b1eeb0582cfe3fcd50fb8123
Alan Gonzalez <argaalan@hotmail.com>
2020-10-16 17:43:28 -0600

    MongoAtlas DB connection and JOI validation

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose')
+const dotenv = require('dotenv');
 
+//Import routes
 const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useUnifiedTopology: true,  useNewUrlParser: true },
+    () => console.log('Connected to db')
+);
+
+//Middleware
+app.use(express.json());
 
 //Route middleware
 app.use('/api/user', authRoute);
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,30 @@
 const router = require('express').Router();
+const User = require('../model/User');
+const {registerValidation, loginValidation} = require('../validation');
 
-router.post('/register', (req, res) => {
-    res.send('Register');
+router.post('/register', async (req, res) => {
+
+    //DATA VALIDATION
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the user is in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists');
+
+    //Create a new user
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    try {
+        const savedUser = await user.save();
+        res.send(savedUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 })
 
 module.exports = router;
